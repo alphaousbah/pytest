204ab44b23846f6f0c6d93f58ee583bae0fda386
alphaousbah <alphaousbah@gmail.com>
2024-06-08 12:00:04 +0200

    [Add] markers

--- tests/test_class.py
+++ tests/test_class.py
@@ -1,8 +1,12 @@
+import pytest
+
+
 class TestClass:
     def test_one(self) -> None:
         x: str = "hello"
         assert "h" in x
 
+    @pytest.mark.xfail  # type: ignore[misc]
     def test_two(self) -> None:
         x: str = "hello"
         assert hasattr(x, "row")
--- tests/test_format_data.py
+++ tests/test_format_data.py
@@ -1,20 +1,13 @@
 from typing import Any
 
-from format_data import format_data_for_display, format_data_for_excel
+import pytest
+
+from format_data import format_data_for_display
 
 
+@pytest.mark.data_test  # type: ignore[misc]
 def test_format_data_for_display(people_data: list[dict[str, Any]]) -> None:
     assert format_data_for_display(people_data) == [
         "Alfonsa Ruiz: Senior Software Engineer",
         "Sayid Khan: Project Manager",
     ]
-
-
-def test_format_data_for_excel(people_data: list[dict[str, Any]]) -> None:
-    assert format_data_for_excel(people_data) == "\n".join(
-        [
-            "given,family,title",
-            "Alfonsa,Ruiz,Senior Software Engineer",
-            "Sayid,Khan,Project Manager",
-        ]
-    )
